e9a7395b148ea119cc076ec81403b7ae4aec878e
Patrick Gorospe <pgorospe@Patricks-MacBook-Air.local>
2020-05-05 14:39:20 -0400

    Adding dockerfile

--- server/index.js
+++ server/index.js
@@ -5,9 +5,13 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(cors());
-app.use(bodyParser());
+app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => {
   console.log(`Received request: ${req.url}`);
   res.json({ message: 'hello-world' });
+});
+
+app.listen(3000, () => {
+  console.log('App now listening on port 3000');
 });
