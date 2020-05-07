dc9f2b577a4ae9cf228068c1a75985228f19beee
Patrick Gorospe <pgorospe@Patricks-MacBook-Air.local>
2020-05-07 10:13:48 -0400

    Abstract out commenting for each hit to BE

--- server/index.js
+++ server/index.js
@@ -1,24 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const Firestore = require('@google-cloud/firestore');
 
 const app = express();
 
+// Config
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  console.log(`Received request: ${req.url}`);
+  next();
+});
+
 // Constants
-const PORT = process.env.$PORT || 3000;
-const HOST = process.env.$HOST || '0.0.0.0';
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
+const firestore = new Firestore({
+  projectId: process.env.GOOGLE_CLOUD_PROJECT,
+});
 
 app.get('/hello', (req, res) => {
-  console.log(`Received request: ${req.url}`);
   res.json({ message: 'hello-world' });
 });
 
 app.get('/', (req, res) => {
-  console.log(`Received request: ${req.url}`);
   res.json({ message: 'Welcome to Undercut!' });
 });
+
+app.post('/message', (req, res) => {
+  console.log(`Posting data.... ${req.body}`);
+  const message = 'successfully saved data';
+
+  res.send(message);
+});
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
